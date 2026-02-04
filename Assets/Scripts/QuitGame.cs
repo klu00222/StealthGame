7f32094be9c5a10d165fe16a2a5eb21bdb8545c5
Alexia Martínez <amartinezla@edu.tecnocampus.cat>
2026-02-04 19:24:35 +0100

    Title and ending

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Update()
+    {
+        if ((Keyboard.current != null) && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Quit();
+        }
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
