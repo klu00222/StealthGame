ab0d93c8f96607af5962db8f2493b9374ec8db93
Arnau Badenas <4bvives@gmail.com>
2026-02-06 18:04:51 +0100

    Make camera movement smoother

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    Vector3 target;
+    private Vector3 target;
+    private Vector3 smoothedPosition;
 
     [SerializeField]
     private Transform player;
 
-    void Start()
+    [SerializeField][Range(1f, 5f)] private float smoothSpeed;
+
+    private void Start()
     {
         target = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+
     }
 
-    void Update()
+    private void Update()
     {
         target.x = player.transform.position.x;
         target.y = player.transform.position.y;
 
-        transform.position = target;
+        smoothedPosition = Vector3.Lerp(transform.position, target, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
     }
 }
