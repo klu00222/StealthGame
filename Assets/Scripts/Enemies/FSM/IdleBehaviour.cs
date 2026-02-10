881ab0ef4ade40ac709b46177efa35c1afdd1d4c
Arnau Badenas <4bvives@gmail.com>
2026-02-10 19:31:48 +0100

    Update idle behaviour

--- Assets/Scripts/Enemies/FSM/IdleBehaviour.cs
+++ Assets/Scripts/Enemies/FSM/IdleBehaviour.cs
@@ -5,46 +5,36 @@ public class IdleBehaviour : StateMachineBehaviour
     [SerializeField]
     private float stayTime = 3.0f;
 
-    private Transform player;
     private float timer;
-    private bool playerClose;
+    private EnemyData data;
 
     private static readonly int IsPatrolling = Animator.StringToHash("isPatrolling");
     private static readonly int IsChasing = Animator.StringToHash("isChasing");
 
-    private void OnEnable()
-    {
-        VisionDetector.OnPlayerDetected += IsPlayerClose;
-    }
-
-    private void OnDisable()
-    {
-        VisionDetector.OnPlayerDetected -= IsPlayerClose;
-    }
-
     public override void OnStateEnter(Animator animator, AnimatorStateInfo state, int layer)
     {
         timer = 0.0f;
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerClose = false;
+        data = animator.GetComponent<EnemyData>();
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo state, int layer)
     {
-        bool timeUp = IsTimeUp();
+        if (data == null)
+        {
+            return;
+        }
 
-        animator.SetBool(IsPatrolling, timeUp);
-        animator.SetBool(IsChasing, playerClose);
-    }
+        if (data.CanSeePlayer())
+        {
+            animator.SetBool(IsChasing, true);
+            return;
+        }
 
-    private bool IsTimeUp()
-    {
         timer += Time.deltaTime;
-        return timer > stayTime;
-    }
+        if (timer >= stayTime)
+        {
+            animator.SetBool(IsPatrolling, true);
+        }
 
-    private void IsPlayerClose()
-    {
-        playerClose = true;
     }
 }
